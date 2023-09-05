06f50ca1a6ea631be18645a1e9eff1dc39e1e4d9
adminuser <adminuser@kali.mvm>
2023-09-05 06:58:01 +0530

    added new_files

--- new_hw.py
+++ new_hw.py
@@ -3,22 +3,41 @@ from prefect.deployments import Deployment
 from prefect.filesystems import S3, GitHub
 #from prefect.task_runners import SequentialTaskRunner, DaskTaskRunner
 
-storage = GitHub.load("file-storage") # load a pre-defined block
+#storage = GitHub.load("file-storage") # load a pre-defined block
 
-@task(name="task_1")
-def task_1(name="task_1"):
-	print(name)
 
-@flow(log_prints=True, name="flow_1")
-def hello_world(task_id: int = 1):
-    if task_id == 1:
-    	task_1()
 
-deployment = Deployment.build_from_flow(
-	flow=hello_world,
-	name="s3-example",
-	version="2",
-	tags=["github"],
-	storage=storage,)
+#deployment = Deployment.build_from_flow(
+#	flow=hello_world,
+#	name="python_api",
+#	version="2",
+#	tags=["github"],
+#	storage=storage,)
 
-deployment.apply()
+#deployment.apply()
+#deployment.run_deployment(
+#	name="flow_1/python_api ",
+
+#)
+
+from prefect import task, flow
+from prefect import get_run_logger
+from typing import Any
+
+
+@task
+def say_hi(user_name: str, question: str, answer: Any) -> None:
+    logger = get_run_logger()
+    logger.info("Hello from Prefect, %s! 👋", user_name)
+    logger.info("The answer to the %s question is %s! 🤖", question, answer)
+
+
+@flow
+def parametrized(
+    user: str = "Marvin", question: str = "ultimate", answer: Any = 42
+) -> None:
+    say_hi(user, question, answer)
+
+
+if __name__ == "__main__":
+    parametrized(user="World")
